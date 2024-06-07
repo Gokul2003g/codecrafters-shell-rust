46ca9d6ee8500b04245a698b6423b9a2d6b6f075
Gokul <75162131+Gokul2003g@users.noreply.github.com>
2024-06-07 21:29:45 +0530

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,11 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim());
+
+        match input.trim() {
+            "exit" => break,
+            "exit 0" => break,
+            _ => println!("{}: command not found", input.trim()),
+        }
     }
 }
