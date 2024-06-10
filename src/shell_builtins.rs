be582c2cd3a56a124ad0f64daeeac2b7eff81558
Gokul <75162131+Gokul2003g@users.noreply.github.com>
2024-06-10 22:32:36 +0530

    cd builtin, home directory

--- src/shell_builtins.rs
+++ src/shell_builtins.rs
@@ -2,9 +2,13 @@ use std::env;
 use std::path::Path;
 
 pub fn change_directory(args: &str) {
-    let path = Path::new(args);
-    if env::set_current_dir(path).is_err() {
-        // cd: /does_not_exist: No such file or directory
+    let path = if args == "~" {
+        dirs::home_dir().expect("Error getting home directory")
+    } else {
+        Path::new(args).to_path_buf()
+    };
+
+    if env::set_current_dir(&path).is_err() {
         println!("cd: {}: No such file or directory", path.display());
     }
 }
